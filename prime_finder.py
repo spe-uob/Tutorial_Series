50a11940345439491ade8bda5686bf2d3e88fb9d
cynical-cheddar <nadroj.taylor99@gmail.com>
2023-08-08 15:21:16 +0100

    p_finder

--- prime_finder.py
+++ prime_finder.py
@@ -1,7 +1,7 @@
 # Python program to display all the prime numbers within an interval
 
-lower = 50
-upper = 100
+lower = 400
+upper = 500
 
 print("Prime numbers between", lower, "and", upper, "are:")
 
