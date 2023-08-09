d14986c59f623798af3b8e2927633f86e1d16663
cynical-cheddar <nadroj.taylor99@gmail.com>
2023-08-09 09:35:45 +0100

    a

--- prime_finder.py
+++ prime_finder.py
@@ -6,10 +6,10 @@ upper = 500
 print("Prime numbers between", lower, "and", upper, "are:")
 
 for num in range(lower, upper + 1):
-   # all prime numbers are greater than 1
-   if num > 1:
-       for i in range(2, num):
-           if (num % i) == 0:
-               break
-       else:
-           print(num)
+   pass
+
+def identity(x):
+    return x
+
+def test_identity():
+    assert identity(3) == 3
